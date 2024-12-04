4cba497bd45660ca80c6bde877c192b3888776fe
vasudhevb <dev.vasu74@gmail.com>
2024-12-04 15:21:18 +0530

    asdfg

--- module/main.tf
+++ module/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"  # Replace with your desired region
 }
 
-module "aws_instance" {
-  source        = "./eccc2"
-  ami_id        = "ami-0453ec754f44f9a4a"  # Replace with a valid AMI ID
-  instance_type = "t2.micro"
-  key_name      = "anuraj"
+module "dhev" {
+  source        = "../eccc2"
+  ami       =   var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
 }
---
+++ module/variable.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  type = string
+  default = "ami-0453ec754f44f9a4a"
+}
+variable "key_name" {
+  type = string
+  default = "anuraj"
+}
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
